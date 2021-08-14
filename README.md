257d1395bdb246eea3a01e3af5e5cb4b5a06c531
HyukJIn <insort@naver.com>
2021-08-15 07:03:02 +0900

    Add basic code

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # study_node_socket_io
- 
+   
+# History  
+npm init  
+npm install express  
+npm install socket.io  
+  
+Create index.js
+Create public/chat.html  
+  
---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const app = express()
+app.use(express.static('public'));
+
+const server = app.listen(3000, () => {
+    console.log('Start Express on 3000 port');
+})
+
+const io = require('socket.io')(server, {
+    log: false,
+    origins: '*:*',
+    pingInterval: 3000,
+    pingTimeout: 5000
+});
+
+io.sockets.on('connection', (socket, opt) => {
+    
+});
