a45dc9753ed7373bc6fb2be593892eaca7c84e64
HyukJIn <insort@naver.com>
2021-08-15 14:04:50 +0900

    not working code

--- index.js
+++ index.js
@@ -15,5 +15,32 @@ const io = require('socket.io')(server, {
 });
 
 io.sockets.on('connection', (socket) => {
+    // init id 
+    socket.emit('display-id', { id: socket.id });
 
+    // init room
+    // console.log('rooms : ', socket.rooms); // Set { <socket.id> }
+    // socket.join("square");
+    // console.log('rooms : ', socket.rooms)
+
+    // socket.on('message', (data, fn) => {
+    //     socket.to(data.roomId).emit('message', {room: data.roomId, msg:data.msg});
+    //     if(fn) fn();
+    // });
+
+    socket.on('join', (roomId, fn) => {
+        console.log('join');
+        console.log('rooms : ', socket.rooms); // Set { <socket.id> }
+        socket.join("square");
+        console.log('rooms : ', socket.rooms)
+
+        if (fn) {
+            fn();
+        }
+    });
+
+    socket.on('message', (data, fn) => {
+        socket.emit('message', {msg:data.msg});
+        fn();
+    });
 });
