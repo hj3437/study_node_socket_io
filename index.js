5f88782309acd7d8c05743ed9cb0fdfd6888416e
HyukJIn <insort@naver.com>
2021-08-15 13:29:51 +0900

    temp code

--- index.js
+++ index.js
@@ -15,5 +15,35 @@ const io = require('socket.io')(server, {
 });
 
 io.sockets.on('connection', (socket) => {
-  
+    socket.emit('message', { msg: socket.id });
+
+    socket.on('join', (roomId, fn) => {
+        if (fn) {
+            fn();
+        }
+
+        console.log(socket.rooms); // Set { <socket.id> }
+        socket.join(roomId);
+        console.log(socket.rooms); // Set { <socket.id>, "room1" }
+    });
+
+    socket.on('leave', function (roomId, fn) {
+        console.log('[server] Leave...');
+        socket.leave(roomId, () => {
+            if (fn)
+                fn();
+        });
+    });
+
+    socket.on('message', (data, fn) => {
+        // Message Data { roomId: 'square', msg: 'Message' }
+        // console.log('[server] Message Data', data);
+        // [server] Message Fn [Function (anonymous)]
+        // console.log('[server] Message Fn', fn);
+        socket.broadcast.to('square').emit('message', { roodId: data.roomId, msg: data.msg });
+
+        if (fn) {
+            fn(data.msg);
+        }
+    });
 });
