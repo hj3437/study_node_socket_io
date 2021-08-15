eeb8813f0c343cffc6cc61f914fb233f0b28a704
HyukJIn <insort@naver.com>
2021-08-15 12:22:27 +0900

    reset codes

--- index.js
+++ index.js
@@ -15,38 +15,5 @@ const io = require('socket.io')(server, {
 });
 
 io.sockets.on('connection', (socket) => {
-    // First Message
-    socket.emit('message', { msg: 'Welcome ' + socket.id });
-
-    socket.on('rooms', function (fn) {
-        if (fn) {
-            // empty = []
-            // Object.keys(socket.rooms);
-
-            // rooms: Map<Room, Set<SocketId>>
-            socket.rooms.forEach(element => {
-                console.log(element);
-                fn(element);
-            });
-        }
-    });
-
-    socket.on('join', (roomName, fn) => {
-        console.log('SERVER JOIN', roomName);
-    });
-
-    socket.on('message', (data, fn) => {
-        socket.broadcast.to(data.room).emit('message', { room: data.room, msg: data.msg });
-
-        if (fn)
-            fn(data.msg);
-    });
-
-    socket.on('direct-message', (socketId, msg) => {
-        console.log('direct-message FN CALLED', Date());
-        console.log('id: ', socketId);
-        console.log('msg: ', msg);
-
-        socket.to(socketId).emit('message', { id: socketId, msg: msg });
-    });
+  
 });
